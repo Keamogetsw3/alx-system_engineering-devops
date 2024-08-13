eca81279ece633cbef6b3cb5ef70b54c5bd85d11
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-08-13 17:53:37 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-function that queries the Reddit API and returns the number of subscribers 
+function that queries the Reddit API and returns the number of subscribers
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers 
+    function that queries the Reddit API and returns the number of subscribers
     """
     url = ("https://api.reddit.com/r/{}/about".format(subreddit))
-    header = {'User-Agent': 'alx_api_advanced'}
+    headers = {'User-Agent': 'alx_api_advanced'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:

632e1883f23442668225e9f68888c51d92dd9745
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-08-15 17:30:52 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,8 +15,7 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        try:
-            return response.json().get("data", {}).get("subscribers", 0)
-        except ValueError:
-            return 0
-    return 0
+        return (0)
+    
+    results = response.json().get("data")
+    return (results.get("subscribers"))

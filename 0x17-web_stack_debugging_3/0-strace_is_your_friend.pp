ca99b1a91eb1bdf660d3234d88b42c0bf5cefb9b
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-08-16 03:22:04 +0200

    Update 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#automate a fix for Apache is returning a 500 error using Puppet
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}

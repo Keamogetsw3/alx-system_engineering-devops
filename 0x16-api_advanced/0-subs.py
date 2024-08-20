2c522c5ba93c27c61f20f05ae9637ec73d8631da
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-08-20 11:43:34 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,17 +3,15 @@
 Function that queries the Reddit API and returns the number of subscribers
 """
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API and returns the number of subscribers
-    """
-    url = "https://api.reddit.com/r/{}/about".format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced/0.1 (by /u/Chemistry_Amu)'}
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 "
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 404:
         return 0
     results = response.json().get("data")

a866ebdb300b69293c1b3634be8c710fd6d72aa1
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-08-20 11:56:28 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
  function that queries the Reddit API and returns the number of subscribers
 """
@@ -11,7 +12,6 @@ def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
         return 0
     response = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/Chemistry_Amu'}).json()
+                     headers={'User-Agent': '0x16-api_advanced:project:V1.0.0 (by /u/Chemistry_Amu'}).json()}
     subs = response.get("data", {}).get("subscribers", 0)
     return subs

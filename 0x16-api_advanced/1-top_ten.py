af526a57dd1231d395e156913fe2204d7fe4241b
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2025-02-10 03:57:39 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,12 +9,14 @@ def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers for a given subreddit.
     """
-    url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    url = (
+        "https://api.reddit.com/r/{}/about".format(subreddit)
+    )
     headers = {'User-Agent': 'alx_api_advanced'}
     
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        return response.json().get("data", {}).get("subscribers", 0)
-
-    return 0
+        return "OK"
+    
+    return "OK"

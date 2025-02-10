9a9df8b3e00a37d827ffba2316f80098c9809610
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2025-02-10 03:53:27 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,16 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Returns the number of subscribers (not active users, total subscribers) for a given subreddit.
+    Returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
     if not isinstance(subreddit, str) or not subreddit:
         return 0
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "0x16-api_advanced:project:V1.0.0 (by /u/Chemistry_Amu)"}
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:V1.0.0 (by /u/Chemistry_Amu)"
+    }
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
